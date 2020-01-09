2aa80300c76608e6d5ea97fca3235de72fd1ddae
Doniyor Ganiev <donik.ganiev05@mail.ru>
2020-01-09 06:44:48 +0300

    Fixed #3

--- bonus.go
+++ bonus.go
@@ -9,14 +9,14 @@ func main() {
 
 func sumOfBonuses(sales []int) int {
 
-	var cash, total int
+	var total int
 	const lineOfBound = 10_000
 	const percent = 5
 	bonusCash := 1
 
 	for i := 0; i < len(sales); i++ {
 		if sales[i] > lineOfBound {
-			cash = sales[i] - lineOfBound
+			cash := sales[i] - lineOfBound
 			bonusCash = cash * percent / 100
 			total += bonusCash
 		}

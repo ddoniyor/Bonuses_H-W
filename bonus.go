55bde2a9bd29eeafe2ae37e663a3ae666adc7dc4
Doniyor Ganiev <donik.ganiev05@mail.ru>
2020-01-09 06:39:33 +0300

    Fixed #2

--- bonus.go
+++ bonus.go
@@ -1,26 +1,24 @@
 package main
 
-import "fmt"
-
 func main() {
 
-	sales := []int{12_000,8_000,15_000,8_000}
-	result := sumOfBonuses(sales)
-	fmt.Println(result)
+	sales := []int{12_000, 8_000, 15_000, 8_000}
+	sumOfBonuses(sales)
+
 }
 
-func sumOfBonuses(sales []int)int{
+func sumOfBonuses(sales []int) int {
 
-	var cash,total int
+	var cash, total int
 	const lineOfBound = 10_000
 	const percent = 5
 	bonusCash := 1
 
-	for i:=0; i<len(sales); i++{
-		if sales[i] > lineOfBound{
-			cash=sales[i]-lineOfBound
-			bonusCash=cash*percent/100
-			total+=bonusCash
+	for i := 0; i < len(sales); i++ {
+		if sales[i] > lineOfBound {
+			cash = sales[i] - lineOfBound
+			bonusCash = cash * percent / 100
+			total += bonusCash
 		}
 	}
 	return total
--- bonus_test.go
+++ bonus_test.go
@@ -5,23 +5,21 @@ import "testing"
 func Test_sumOfBonuses(t *testing.T) {
 
 	tests := []struct {
-		name string
+		name  string
 		sales []int
-		want int
+		want  int
 	}{
 		// TODO: Add test cases.
 
-			// TODO: Add test cases.
-			{"Correct sum of all Bonuses",[]int{12_000,15_000},350},
-			{"Without Bonuses",[]int{8_000,8_000},0},
-			{"Bound line",[]int{10_000,10_000},0},
-
+		// TODO: Add test cases.
+		{"Correct sum of all Bonuses", []int{12_000, 15_000}, 350},
+		{"Without Bonuses", []int{8_000, 8_000}, 0},
+		{"Bound line", []int{10_000, 10_000}, 0},
 	}
 	for _, test := range tests {
-		got :=sumOfBonuses(test.sales)
-		if got != test.want{
-			t.Error("Bonus test:",test.name,"got:",got,"want:",test.want)
+		got := sumOfBonuses(test.sales)
+		if got != test.want {
+			t.Error("Bonus test:", test.name, "got:", got, "want:", test.want)
 		}
 	}
 }
-
